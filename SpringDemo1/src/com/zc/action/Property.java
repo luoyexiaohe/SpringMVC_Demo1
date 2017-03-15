ab86ef00888624fcfba3669f775b2bf6dd30872d
John <John@JohnPC>
2017-03-15 13:29:28 +0800

    更新了服务器的jar包

--- SpringDemo1/src/com/zc/action/Property.java
+++ SpringDemo1/src/com/zc/action/Property.java
@@ -6,7 +6,6 @@ import java.util.ResourceBundle;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.naming.resources.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
